0185aecefbbdd129c3ab50076f51582dfb4d4b9a
Bearded Avenger <email@nickhaskins.com>
2015-11-10 10:17:39 -0500

    base test

---
+++ cgc-core--markets.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+*	Plugin Name: CGC Core Markets
+*	Description: Markets Core plugin that pulls functionality from CGC Core base plugin
+*
+*
+*/
+
+require( CGC5_CORE_DIR.'/public/includes/user-functions.php' );
+
+
+$test = cgc_is_user_disabled();
+
