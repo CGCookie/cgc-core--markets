b29ef2a2c3cb339696af0ad0e1279c0381528b57
Bearded Avenger <email@nickhaskins.com>
2015-11-10 11:02:25 -0500

    super duper corndog beta 1

--- cgc-core--markets.php
+++ cgc-core--markets.php
@@ -3,12 +3,43 @@
 /**
 *	Plugin Name: CGC Core Markets
 *	Description: Markets Core plugin that pulls functionality from CGC Core base plugin
+*	Version: 0.1 super duper corndog beta
 *
 *
+*
+*	Notes: - the theme constants need to be moved into the autoloader so that they're available when we load cgcfive js on markets
+*			- the style enqueued for  edu needs to be moved into the theme
+*			- cgc-core needs to be site activated on main site and not network wide
+*			- cgc-activity probably needs to be network activated
 */
 
-require( CGC5_CORE_DIR.'/public/includes/user-functions.php' );
+class cgcCoreMarkets {
+
+	public function __construct(){
+
+		require( CGC5_CORE_DIR.'/includes/class.api-route-factory.php' );
+		require( CGC5_CORE_DIR.'/includes/functions--rest-api.php' );
+		require( CGC5_CORE_DIR.'/includes/class.process-live-notify.php' );
+		require( CGC5_CORE_DIR.'/includes/class.live-notify-script.php' );
+		require( CGC5_CORE_DIR.'/includes/class.user-api.php' );
+		require( CGC5_CORE_DIR.'/public/includes/class.assets.php' );
+		require( CGC5_CORE_DIR.'/public/includes/user-functions.php' );
+
+		add_action( 'wp_footer', 				array( 'cgcLiveNotifyScript', 'script' ) );
+		add_action( 'wp_head', 					array( 'cgcFiveAssets', 'typekit' ) );
+		add_action( 'wp_enqueue_scripts',	 	array( 'cgcFiveAssets', 'scripts' ) );
+		add_action( 'rest_api_init', 			array( 'cgcApiRouteFactory', 'register_routes' ) );
+		add_action( 'rest_api_init', 			array($this, 'rest_api'), 5 );
+	}
+
+	public static function rest_api() {
+		cgcApiRouteFactory::create( 'cgcVideoTrackingProcess' );
+		cgcApiRouteFactory::create( 'cgcProcessLiveNotify', array( 'GET', 'POST') );
+	}
+}
+new cgcCoreMarkets();
 
 
-$test = cgc_is_user_disabled();
+
+
 
